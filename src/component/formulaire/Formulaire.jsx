2ced47460aa7ecbd9d1032e824c3375233f88976
Laura Carazo <laura@self-couture.com>
2020-12-10 21:59:32 +0100

    quelques fields en plus

--- src/component/formulaire/Formulaire.jsx
+++ src/component/formulaire/Formulaire.jsx
@@ -2,8 +2,11 @@ import { Field, Form, Formik } from 'formik';
 import React from 'react';
 
 const initialValues = {
-	firstName : "",
-	lastName : "",
+  status : "",
+  firstName : "",
+  lastName : "",
+  birthday : "",
+  email: ""
 }
 
 class Formulaire extends React.Component {
@@ -19,11 +22,71 @@ class Formulaire extends React.Component {
            onSubmit={this.submit} 
           >
            {() => (
-             <Form>
-               <Field type="text" name="firstName"/>
-               <Field type="text" name="lastName"/>
+              <div className="container border p-3 m-3">
+                
+                  <Form>
+                    <div className="">
+                      <div className="mb-3 d-flex flex-column">
+                        <div className="flex-row">
+                          <label className="me-2">
+                          <Field  
+                              className="me-2"                        
+                              type = "radio"
+                              name = "status"
+                              value = "mr"
+                          /> 
+                              Mr  
+                          </label>
+                          <label className="me-2">
+                          <Field  
+                              className="me-2"                        
+                              type = "radio"
+                              name = "status"
+                              value = "ms"
+                          /> 
+                              Ms  
+                          </label>
+                          <label className="me-2">
+                          <Field  
+                              className="me-2"                        
+                              type = "radio"
+                              name = "status"
+                              value = "ms"
+                          /> 
+                              N/A  
+                          </label>
+                        </div>
+                        
+                      
+                        <label htmlFor="firstName" className="form-label">Firstname</label>
+                        <Field 
+                          type="text" 
+                          name="firstName" 
+                          className="form-control"
+                          placeholder = "Firstname"  
+                        />  
+                      </div>
+                
+                      <div className="mb-3 d-flex flex-column">
+                        <label htmlFor="lastName" className="form-label">Lastname</label>
+                        <Field 
+                          type="text" 
+                          name="lastName" 
+                          className="form-control"
+                          placeholder = "Lastname"
+                        />
+                      </div>
+                    </div>
+                    <div className="">
+                      
+                    </div>
+                    
+              
 
-             </Form>
+                    <button type="submit" className= "btn btn-success mt-3">Submit</button>
+                  </Form>
+              </div>
+              
            )
 
            }
